41c7c12e594bb9e5a8124f2c96cb935a4701cae5
Kosisochukwu Azubogu <azuboguko@gmail.com>
2026-02-08 15:17:10 +0100

    feat: Implement login functionality with routing and login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import LecturerEvaluationFormPage from "./pages/LecturerEvaluationFormPage"
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="min-h-screen p-5">
-      <LecturerEvaluationFormPage />
+      <Routes>
+        <Route path="/" element={<Navigate to="/evaluate" />} />
+        <Route
+          path="/evaluate"
+          element={<LecturerEvaluationFormPage />}
+        ></Route>
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        ></Route>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginComponent from "../components/LoginComponent"
+
+const LoginPage = () => {
+  return (
+    <LoginComponent/>
+  )
+}
+
+export default LoginPage
