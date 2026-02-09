b59262f2b99f3d6d3291275b478a48266fa34267
Kosisochukwu Azubogu <azuboguko@gmail.com>
2026-02-09 07:29:52 +0100

    feat: Add necessary models for evaluation app
    Added Lecturer, Student, and EvaluationMetadata models to the backend. Updated package.json and package-lock.json to include necessary dependencies to be used in the project's lifecycle. These models will be used to store and manage data related to lecturers, students, and evaluation metadata in the application.

---
+++ backend/src/models/Lecturer.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const Lecturer = new mongoose.Schema(
+  {
+    lecturerName: {
+      type: String,
+      required: true,
+    },
+    ratedCourses: [
+      {
+        courseCode: { type: String, required: true },
+        courseTitle: { type: String, required: true },
+        avgRating: {
+          type: Number,
+          min: 1,
+          max: 5,
+          required: true,
+        },
+      },
+    ],
+    ovrRating: {
+      type: Number,
+      required: true,
+    },
+    numOfRatingsForOvrRating: {
+      type: Number,
+      required: true,
+    },
+    deptRating: {
+      type: Number,
+      required: true,
+    },
+    numOfRatingsForDeptRating: {
+      type: Number,
+      required: true,
+    },
+    facultyRating: {
+      type: Number,
+      required: true,
+    },
+    numOfRatingsForFacultyRating: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Lecturer", Lecturer);
